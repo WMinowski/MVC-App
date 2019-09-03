1cef6072e866f9664de8d39d1756625d9a9ba2b7
WMinowski <wminowski@gmail.com>
2019-09-03 15:12:48 +0300

    DB included, EF Core models created

---
+++ MVC-App/Models/RelationAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_App.Models
+{
+    public class RelationAddress
+    {
+        public int RelationId { get; set; }
+
+        public Relation Relation { get; set; }
+
+        public int CountryId { get; set; }
+
+        public Country Country { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+
+        public string PostalCode { get; set; }
+
+        public int Number { get; set; }
+    }
+}
