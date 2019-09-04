84760b0b80204ec11663dc59178fab0cf3796400
WMinowski <wminowski@gmail.com>
2019-09-04 14:42:44 +0300

    deployed EF, created first view

--- MVC-App/Controllers/HomeController.cs
+++ MVC-App/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_App.Domain.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,6 +9,8 @@ namespace MVC_App.Controllers
 {
     public class HomeController : Controller
     {
+        public readonly testEntities db = new testEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -26,5 +29,21 @@ namespace MVC_App.Controllers
 
             return View();
         }
+
+        public ActionResult RelationView()
+        {
+            //using (testEntities db = new testEntities())
+            //{
+            //    //tblRelation relation = db.tblRelation.Where(p => Int32.Parse(p.Id.ToString()) == id).FirstOrDefault();
+            //    return View(db.tblRelation);
+            //}
+            return View(db.tblRelation);
+        }
+
+        //protected override void Dispose(bool disposing)
+        //{
+        //    db.Dispose();
+        //    base.Dispose(disposing);
+        //}
     }
 }
