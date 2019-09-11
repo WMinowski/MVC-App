4d89c5ea3ee9d80102beac3f184ee02b15be1b0f
WMinowski <wminowski@gmail.com>
2019-09-11 17:27:13 +0300

    validation restrictions applied

--- MVC-App/Domain.Models/RelationViewModel.cs
+++ MVC-App/Domain.Models/RelationViewModel.cs
@@ -17,35 +17,37 @@ namespace MVC_App.Domain.Models
 
         public bool IsDisabled { get; set; }
 
-        [Required]
-        [StringLength(128)]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128, MinimumLength = 2, ErrorMessage = "Input should be between 2 and 128 characters")]
         public string Name { get; set; }
 
-        [Required]
-        [StringLength(128)]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128, MinimumLength = 2, ErrorMessage = "Input should be between 2 and 128 characters")]
         public string FullName { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         [DataType(DataType.PhoneNumber, ErrorMessage = "Invalid Phone Number")]
         public string TelephoneNumber { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         [DataType(DataType.EmailAddress, ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
 
         public Guid CountryId { get; set; }
 
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128, MinimumLength = 2, ErrorMessage = "Input should be between 2 and 128 characters")]
         public string CountryName { get; set; }
 
-        [Required]
-        [StringLength(128)]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128, ErrorMessage = "Input should be less than 128 characters")]
         public string City { get; set; }
 
-        [Required]
-        [StringLength(128)]
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(128, ErrorMessage = "Input should be less than 128 characters")]
         public string Street { get; set; }
 
-        [Required]
+        [Required(AllowEmptyStrings = false)]
         [DataType(DataType.PostalCode, ErrorMessage = "Invalid Postal Code")]
         public string PostalCode { get; set; }
 
